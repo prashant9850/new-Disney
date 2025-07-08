ced463ee0417ea0d35d5eed17c895dafac2a5a52
prashant9850 <prashantsureshjadhav409@gmail.com>
2025-07-08 15:10:08 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="w-full flex justify-center">
+        <p className="text-5xl text-red-600">hello</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
